f25cba0d53998786af1b7eb36b9285485cb43d9b
Toukiloglou <toukiloglou@gmail.com>
2020-07-07 11:47:19 +0300

    Simple ECS

---
+++ ECS/Assets/Scripts/ConvertSpawner.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ConvertSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject gameObjectPrefab;
+    [SerializeField] int xSize = 10;
+    [SerializeField] int ySize = 10;
+    [Range(0.1f, 5.0f)] [SerializeField] float spacing = 1.5f;
+
+    private Entity entityPrefab;    
+    private EntityManager entityManager;
+
+    void Start()
+    {
+        
+        entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        entityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(
+                        gameObjectPrefab,
+                        GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null));
+
+        InstatiateGrid(xSize, ySize, spacing);
+            
+    }
+
+    private void InstantiateEntity(float3 position) {
+        Entity myEntity = entityManager.Instantiate(entityPrefab);
+        entityManager.SetComponentData(myEntity, new Translation {
+            Value = position 
+        });
+    }
+
+
+    private void InstatiateGrid(int dimX, int dimY, float spacing = 1f) {
+        for (int i = 0; i < dimX; i++) {
+            for (int j = 0; j < dimY; j++) {
+                InstantiateEntity(new float3(i * spacing, j * spacing, 0f));
+            }
+        }
+    }
+
+
+}
---
+++ ECS/Assets/Scripts/JobExample.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+
+public class JobExample : MonoBehaviour
+{
+    
+    void Start()
+    {
+        DoExample();
+    }
+
+    private void DoExample() {
+
+        NativeArray<float> resultArray = new NativeArray<float>(1, Allocator.TempJob);
+
+        SimpleJob myJob = new SimpleJob {
+            a = 5f,
+            result = resultArray
+        };
+
+        JobHandle handle = myJob.Schedule();
+
+        handle.Complete();
+
+        float resultingValue = resultArray[0];
+        Debug.Log(resultingValue);
+
+        resultArray.Dispose();
+    }
+
+    private struct SimpleJob : IJob {
+
+        public float a;
+        public NativeArray<float> result;        
+        public void Execute() {
+            result[0] = a;
+        }
+    }
+
+
+}
